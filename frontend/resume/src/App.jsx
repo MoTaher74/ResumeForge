224b0e7f66dbd02fd909ee66665b8982d29f0b1f
mo <motaherr7431@gmail.com>
2025-11-16 02:18:06 +0200

    Initial MERN project setup

---
+++ frontend/resume/src/App.jsx
@@ -0,0 +1,10 @@
+const App =() =>{
+
+  return(
+    <>
+    <h1 className="m-4 text-xl">app</h1>
+    </>
+  )
+}
+
+export default App;
---
+++ frontend/resume/src/pages/Home/CreateResumeForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CreateResumeForm() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CreateResumeForm ;
